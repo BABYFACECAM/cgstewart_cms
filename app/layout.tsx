d918d6020964d525e85d07640701c507d1fdfa60
Cameron Stewart <newrgm@NEWRGM.attlocal.net>
2024-08-31 13:24:37 -0500

    fixed layout issues and mobile responsiveness .. ready to add amplify data and fetch from db

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,9 +19,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={`${inter.className} flex flex-col min-h-screen`}>
         <Navbar />
-        <main className="flex min-h-screen m-2">{children}</main>
+        <main className="flex-grow px-2">{children}</main>
         <Footer />
       </body>
     </html>
--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -1,12 +1,14 @@
 export default function Footer() {
   return (
-    <footer className="footer  bottom-0 footer-center bg-base-300 text-base-content p-4">
-      <aside>
-        <p>
+    <footer className="footer footer-center bg-base-300 text-base-content p-4 w-full">
+      <aside className="w-full max-w-full px-4">
+        <p className="text-center break-words">
           Copyright © {new Date().getFullYear()} - All right reserved by CG
           Stewart
         </p>
-        <p>Made with ❤️ & Next.js, TailwindCSS, DaisyUI, and TursoDB </p>
+        <p className="text-center break-words">
+          Made with ❤️ & Next.js, TailwindCSS, DaisyUI, and AWS Amplify Gen 2
+        </p>
       </aside>
     </footer>
   );
